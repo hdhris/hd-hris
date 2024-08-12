a0f3c0a02132b9ec7066abc7809c8cdf9e052ca4
XJohnC <97450112+code-with-john@users.noreply.github.com>
2024-08-13 01:45:22 +0800

    fixing the sidebar

--- components/sidebar/SideBarItem.tsx
+++ components/sidebar/SideBarItem.tsx
@@ -1,36 +1,56 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Text from "@/components/common/typography/Text";
-import React, { ReactNode, useEffect, useState } from "react";
-import {cn} from "@nextui-org/react";
+import React, { ReactNode } from "react";
+import {cn, Tooltip} from "@nextui-org/react";
+import { usePathname } from "next/navigation";
 
 interface SidebarItem {
     label: string;
     href: string;
     icon?: ReactNode;
+    showLabel: boolean;
 }
 
+export default function SideBarItem({ label, href, icon, showLabel }: SidebarItem) {
+    const pathname = usePathname();
+    const isActive = pathname === href;
 
-export default function SideBarItem({ label, href, icon }: SidebarItem) {
-    const [isActive, setIsActive] = useState(false);
+    const linkContent = (
+        <Link
+            href={href}
+            className={cn(
+                "flex items-center p-2 min-w-14 max-w-52 text-gray-900 rounded-lg group",
+                isActive ? "font-semibold bg-primary rounded" : "hover:bg-primary/25 hover:rounded"
+            )}
+        >
+            {icon && (
+                <span
+                    className={cn(
+                        "text-gray-500 transition duration-75",
+                        isActive ? "text-white font-semibold" : "group-hover:text-gray-900"
+                    )}
+                >
+                    {icon}
+                </span>
+            )}
+            {showLabel && (
+                <Text className={cn("ms-3 text-sm text-nowrap", isActive ? "text-white" : "group-hover:text-primary")}>
+                    {label}
+                </Text>
+            )}
+        </Link>
+    );
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setIsActive(window.location.pathname === href);
-        }
-    }, [href]);
     return (
         <li>
-            <Link href={href}
-                  className={cn("flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-primary/25 hover:rounded dark:hover:bg-slate-200 group", isActive && 'font-semibold bg-primary rounded')}>
-                {icon && (
-                    <span
-                        className={cn("text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white", isActive && 'text-white font-semibold')}>
-          {icon}
-        </span>
-                )}
-                <Text className={cn("ms-3 text-sm group-hover:text-primary" , isActive && 'text-white')}>{label}</Text>
-            </Link>
+            {showLabel ? (
+                linkContent
+            ) : (
+                <Tooltip content={label} placement="left">
+                    {linkContent}
+                </Tooltip>
+            )}
         </li>
     );
 }
