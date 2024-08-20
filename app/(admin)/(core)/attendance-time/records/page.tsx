412e6322e55db8e4a3aac380fc6a2030e158fa78
MNizamD <ndaatumanong05@gmail.com>
2024-08-20 14:27:42 +0800

    MM

--- app/(admin)/(core)/attendance-time/records/page.tsx
+++ app/(admin)/(core)/attendance-time/records/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Calendar } from "@nextui-org/react";
+import { parseDate } from "@internationalized/date";
 
 export default function page() {
-  return <div>Records</div>;
+  return (
+    <>
+      <div>Records</div>
+      <Calendar
+        aria-label="Date (Uncontrolled)"
+        defaultValue={parseDate("2020-02-03")}
+      />
+    </>
+  );
 }
