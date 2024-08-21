2c8e3bd71177e66ff706cdb9f790d54254c56323
MNizamD <ndaatumanong05@gmail.com>
2024-08-22 01:15:29 +0800

    att record designed

--- app/(admin)/(core)/attendance-time/records/page.tsx
+++ app/(admin)/(core)/attendance-time/records/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 import React from "react";
-import { Calendar } from "@nextui-org/react";
+import {
+  Avatar,
+  Calendar,
+  Card,
+  CardBody,
+  CardHeader,
+  Chip,
+  Divider,
+} from "@nextui-org/react";
 import {
   Table,
   TableHeader,
@@ -32,6 +40,12 @@ const columns = [
 ];
 // Define your dummy data
 // key: "1", Add keys for unique identity
+const avatar = [
+  { key: "1", src: "https://i.pravatar.cc/150?u=a042581f4e29026024d" },
+  { key: "2", src: "https://i.pravatar.cc/150?u=a04258a2462d826712d" },
+  { key: "3", src: "https://i.pravatar.cc/150?u=a042581f4e29026704d" },
+  { key: "4", src: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
+];
 const dummyData = [
   { key: "1", name: "Tony Reichert", status: "TIME IN", time: "8:30 am" },
   { key: "2", name: "Zoey Lang", status: "TIME OUT", time: "8:29 am" },
@@ -51,10 +65,19 @@ const generateData = (data: any, times: any) => {
 };
 
 export default function Page() {
-  let [value, setValue] = React.useState(parseDate("2024-03-07"));
+  const today = new Date();
+  let [value, setValue] = React.useState(
+    parseDate(
+      `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+        2,
+        "0"
+      )}-${String(today.getDate()).padStart(2, "0")}`
+    )
+  );
   // const items = generateData(dummyData, 10); // Generate data repeated 10 times
   const items = dummyData; // Generate data repeated 10 times
   const [selectedKey, setSelectedKey] = React.useState<any>("");
+  const selectedItem = dummyData.find((item) => item.key === selectedKey);
   return (
     <div className="flex flex-row p-1 gap-1">
       <Table
@@ -81,21 +104,119 @@ export default function Page() {
               className={`${selectedKey === row.key ? "bg-gray-300" : ""}`}
             >
               {(columnKey) => (
-                <TableCell>{getKeyValue(row, columnKey)}</TableCell>
+                <TableCell>
+                  {columnKey === "name" ? (
+                    <div className="flex items-center space-x-2">
+                      <Avatar
+                        src={avatar.find((item) => item.key === row.key)?.src}
+                      />
+                      <p>{getKeyValue(row, columnKey)}</p>
+                    </div>
+                  ) : columnKey === "status" ? (
+                    <Chip
+                      className="capitalize"
+                      color="primary"
+                      size="sm"
+                      variant="flat"
+                    >
+                      {getKeyValue(row, columnKey)}
+                    </Chip>
+                  ) : (
+                    getKeyValue(row, columnKey)
+                  )}
+                </TableCell>
               )}
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-1">
         <Calendar
-          classNames={{ cell: "text-sm" }}
+          classNames={{ cell: "text-sm", gridBodyRow: "first:mt-0 -mt-1" }}
           className="h-fit"
           aria-label="Date (Controlled)"
           showMonthAndYearPickers
           value={value}
           onChange={setValue}
         />
+        <Card>
+          <CardHeader className="flex gap-1">
+            <div className="flex flex-col">
+              <p className="text-md">
+                {selectedItem ? selectedItem.name : "No selected"}
+              </p>
+              <p className="text-small text-default-500">On time</p>
+            </div>
+          </CardHeader>
+          <CardBody className="-mt-6">
+            <Table
+              removeWrapper
+              hideHeader
+              aria-label="Example static collection table"
+            >
+              <TableHeader
+                columns={[
+                  {
+                    key: "time",
+                    label: "TIME",
+                  },
+                  {
+                    key: "punch",
+                    label: "PUNCH",
+                  },
+                  {
+                    key: "status",
+                    label: "STATUS",
+                  },
+                ]}
+              >
+                {(column) => (
+                  <TableColumn key={column.key}>{column.label}</TableColumn>
+                )}
+              </TableHeader>
+              <TableBody
+                emptyContent={"No records for this day."}
+                items={[
+                  { key: "1", time: "8:30 am", punch: "IN", status: "EARLY" },
+                  {
+                    key: "2",
+                    time: "12:00 pm",
+                    punch: "OUT",
+                    status: "ONTIME",
+                  },
+                  { key: "3", time: "1:30 pm", punch: "IN", status: "LATE" },
+                  { key: "4", time: "4:58 pm", punch: "OUT", status: "EARLY" },
+                ]}
+              >
+                {(row) => (
+                  <TableRow
+                    key={row.key}
+                    className={`${
+                      selectedKey === row.key ? "bg-gray-300" : ""
+                    }`}
+                  >
+                    {(columnKey) => (
+                      <TableCell className="py-1.5">
+                        {columnKey === "status" ? (
+                          <Chip
+                            className="capitalize"
+                            color="primary"
+                            size="sm"
+                            variant="flat"
+                          >
+                            {getKeyValue(row, columnKey)}
+                          </Chip>
+                        ) : (
+                          getKeyValue(row, columnKey)
+                        )}
+                      </TableCell>
+                    )}
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
