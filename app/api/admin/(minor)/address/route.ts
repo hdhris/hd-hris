f830a1cc4128ae2e2b47a7ee9312d89a3afaaaf8
MNizamD <ndaatumanong05@gmail.com>
2024-09-11 01:27:16 +0800

    embedded address data for faster loading

--- app/api/admin/(minor)/address/route.ts
+++ app/api/admin/(minor)/address/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import addressJson from './address.json';
 
 export async function GET(req: Request) {
   try {
     // const { searchParams } = new URL(req.url);
     // const code = parseInt(searchParams.get('parent_code') as string);
-    const addresses = await prisma.ref_addresses.findMany();
-    return NextResponse.json(addresses);
+    // const addresses = await prisma.ref_addresses.findMany();
+    return NextResponse.json(addressJson);
   } catch (error) {
     console.error("Error fetching addresses:", error);
     return NextResponse.json(
