fc63bcd49cf402bd71ef34611f9ee4e1587bbb91
MNizamD <ndaatumanong05@gmail.com>
2024-09-23 02:03:03 +0800

    quicky

--- lib/utils/toGMT8.ts
+++ lib/utils/toGMT8.ts
@@ -1,3 +1,10 @@
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export function toGMT8(value: string | Date): Date {
     let date: Date;
   
@@ -5,10 +12,12 @@ export function toGMT8(value: string | Date): Date {
       // Check if the input is a time string in the format "HH:mm:ss"
       const timeRegex = /^\d{2}:\d{2}:\d{2}$/;
       if (timeRegex.test(value)) {
-        const now = new Date(); // Get the current date
-        const [hours, minutes, seconds] = value.split(":").map(Number);
-        date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, seconds);
-        // date = new Date(`1970-01-01T${value}:00Z`)
+        // const now = new Date(); // Get the current date
+        // const [hours, minutes, seconds] = value.split(":").map(Number);
+        // date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, seconds);
+        // date = new Date(`1970-01-01T${value}.921Z`)
+        let dayDate: dayjs.Dayjs = dayjs(`1970-01-01T${value}`).tz('Asia/Manila');
+        date = new Date(dayDate.format());
       } else {
         date = new Date(value); // Assume it's an ISO date string
       }
@@ -18,8 +27,6 @@ export function toGMT8(value: string | Date): Date {
   
     const offset = 8 * 60; // GMT+8 in minutes
     const gmt8Time = new Date(date.getTime() + offset * 60 * 1000);
-
-    return new Date(Date.UTC(gmt8Time.getUTCFullYear(), gmt8Time.getUTCMonth(), gmt8Time.getUTCDate(), gmt8Time.getUTCHours(), gmt8Time.getUTCMinutes(), gmt8Time.getUTCSeconds()));
+    return gmt8Time;
   }
-
   
