a2dd4012459481baa538d4c45fa4f469f4de4e0a
TechstyFrance <francejay724@gmail.com>
2024-10-30 05:36:41 +0800

    removing breadcrumbs

--- app/(admin)/(core)/employeemanagement/layout.tsx
+++ app/(admin)/(core)/employeemanagement/layout.tsx
@@ -1,69 +1,34 @@
-"use client";
-import { ReactNode } from "react";
-import NavigationTabs, { TabItem } from "@/components/common/tabs/NavigationTabs";
-import BreadcrumbComponent from "@/components/common/breadcrumb";
-import { usePathname } from "next/navigation";
+import React from 'react';
+import NavigationTabs, {TabItem} from "@/components/common/tabs/NavigationTabs";
 
-type TabKeys =
-  | "employees"
-  | "suspend"
-  | "resign"
-  | "departments"
-  | "jobposition";
-//
-
-function RootLayout({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-  const activeTab = pathname.includes("suspend")
-    ? "suspend"
-    : pathname.includes("resign")
-    ? "resign"
-    : pathname.includes("departments")
-    ? "departments"
-    : pathname.includes("jobposition")
-    ? "jobposition"
-    : "employees";
-  const tabs: TabItem[] = [
-    {
-      key: "employees",
-      title: "Employees",
-      path: "employees",
-    },
-    // {
-    //   key: "suspend",
-    //   title: "Suspend",
-    //   path: "suspend",
-    // },
-    // {
-    //   key: "resign",
-    //   title: "Resign",
-    //   path: "resign",
-    // },
-    {
-      key: "departments",
-      title: "Departments",
-      path: "departments",
-    },
-
-    {
-      key: "jobposition",
-      title: "Job Positions",
-      path: "jobposition",
-    },
-    {
-      key: "branch",
-      title: "Branch",
-      path: "branch",
-    },
-  ];
-
-  return (
-    <div className="flex flex-col gap-2">
-      <NavigationTabs tabs={tabs} basePath="employeemanagement">
-        {children}
-      </NavigationTabs>
-    </div>
-  );
+function RootLayout({children}: { children: React.ReactNode }) {
+    const tabs: TabItem[] = [
+      {
+              key: "employees",
+              title: "Employees",
+              path: "employees",
+            },
+            {
+              key: "departments",
+              title: "Departments",
+              path: "departments",
+            },
+            {
+              key: "jobposition",
+              title: "Job Positions",
+              path: "jobposition",
+            },
+            {
+              key: "branch",
+              title: "Branch",
+              path: "branch",
+            },
+    ]
+    return (
+        <NavigationTabs tabs={tabs} basePath="employeemanagement">
+            {children}
+        </NavigationTabs>
+    );
 }
 
 export default RootLayout;
