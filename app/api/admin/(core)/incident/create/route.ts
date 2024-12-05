388309a8932b5a62c2419e5b625cbe6163f655b7
MNizamD <ndaatumanong05@gmail.com>
2024-12-05 12:22:26 +0800

    last changes

--- app/api/admin/(core)/incident/create/route.ts
+++ app/api/admin/(core)/incident/create/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/prisma";
+import { toGMT8 } from "@/lib/utils/toGMT8";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
-    console.log(body);
-    // await prisma.dim_incident_reports.create({
-    //     data: {
-    //         ...body,
-    //     },
-    // })
+    // console.log(body);
+    await prisma.dim_incident_reports.create({
+        data: {
+          created_at: toGMT8().toISOString(),
+          updated_at: toGMT8().toISOString(),
+          ...body,
+        },
+    })
     return NextResponse.json({ status: 200 });
   } catch (error) {
     console.error("Error:", error); // Log the error for debugging
--- components/admin/incident/reports/incident-drawer.tsx
+++ components/admin/incident/reports/incident-drawer.tsx
@@ -5,184 +5,218 @@ import FormFields from "@/components/common/forms/FormFields";
 import { Form } from "@/components/ui/form";
 import { toast } from "@/components/ui/use-toast";
 import { UserEmployee } from "@/helper/include-emp-and-reviewr/include";
+import { useEmployeeId } from "@/hooks/employeeIdHook";
 import { getEmpFullName } from "@/lib/utils/nameFormatter";
 import { toGMT8 } from "@/lib/utils/toGMT8";
 import { useQuery } from "@/services/queries";
 import { IncidentReport } from "@/types/incident-reports/type";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Avatar, Tooltip } from "@nextui-org/react";
 import axios from "axios";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 interface IncidentDrawerProps {
-  selected: IncidentReport | null;
-  isOpen: boolean;
-  onClose: (val: boolean) => void;
-  // isSubmitting: boolean;
+    selected: IncidentReport | null;
+    isOpen: boolean;
+    onClose: (val: boolean) => void;
+    // isSubmitting: boolean;
 }
 function IncidentDrawer({
-  selected,
-  isOpen,
-  onClose,
+    selected,
+    isOpen,
+    onClose,
 }: // isSubmitting,
 IncidentDrawerProps) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { data, isLoading } = useQuery<UserEmployee[]>("/api/admin/utils/get-employee-search");
-  const employees = useMemo(() => {
-    if (data) {
-      return data.map((emp) => ({
-        id: emp.id,
-        name: getEmpFullName(emp),
-        picture: emp.picture,
-        department: emp.ref_departments?.name,
-      }));
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const { data, isLoading } = useQuery<UserEmployee[]>("/api/admin/utils/get-employee-search");
+    const userID = useEmployeeId();
+    const employees = useMemo(() => {
+        if (data) {
+            return data.map((emp) => ({
+                id: emp.id,
+                name: getEmpFullName(emp),
+                picture: emp.picture,
+                department: emp.ref_departments?.name,
+            }));
+        }
+
+        return [];
+    }, [data]);
+    const formSchema = z.object({
+        employee_id: z.number().optional(),
+        occurance_date: z.string(),
+        severity: z.enum(["minor", "major", "critical"]),
+        type: z.string().min(1, "Enter the type of the incident"),
+        location: z.string().min(1, "Enter the location where the incident occured"),
+        description: z.string().optional(),
+        comments: z.string().optional(),
+        reported_by: z.number(),
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {},
+        values: selected || undefined,
+    });
+
+    const formReset = useCallback(() => {
+        if (form) {
+            form.reset({
+                comments: "",
+                description: "",
+                location: "",
+                occurance_date: toGMT8().toISOString(),
+                severity: "minor",
+                type: "",
+                employee_id: undefined,
+                reported_by: userID,
+            });
+        }
+    }, [form]);
+
+    useEffect(() => {
+        if (!selected && userID) {
+          formReset();
+        }
+    }, [selected, formReset, userID]);
+
+    async function onsubmit(value: z.infer<typeof formSchema>) {
+        setIsSubmitting(true);
+        try {
+            await axios.post("/api/admin/incident/create", value);
+            toast({
+                title: "Incident filed successfully!",
+                variant: "success",
+            });
+        } catch (error) {
+            toast({
+                title: "An error has occured",
+                description: String(error),
+                variant: "danger",
+            });
+        }
+        setIsSubmitting(false);
     }
 
-    return [];
-  }, [data]);
-  const formSchema = z.object({
-    employee_id: z.number().optional(),
-    occurance_date: z.date(),
-    severity: z.enum(["minor", "major", "critical"]),
-    type: z.string(),
-    location: z.string(),
-    description: z.string().optional(),
-    comments: z.string().optional(),
-    reported_by: z.number(),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
-
-  useEffect(() => {
-    if (selected) {
-      form.reset({
-        comments: selected?.comments,
-        description: selected?.description,
-        location: selected?.location,
-        occurance_date: toGMT8(selected?.occurance_date).toDate(),
-        severity: selected?.severity,
-        type: selected?.type,
-        employee_id: selected?.employee_id,
-      });
-    } else {
-      form.reset({
-        comments: "",
-        description: "",
-        location: "",
-        occurance_date: toGMT8().toDate(),
-        severity: "minor",
-        type: "",
-        employee_id: undefined,
-      });
-    }
-  }, [selected, form]);
-
-  async function onsubmit(value: z.infer<typeof formSchema>) {
-    // alert("Submitting");
-    console.log(value);
-    try {
-      await axios.post("/api/admin/incident/create", value);
-      toast({
-        title: "Filed",
-        description: "Incident filed successfully!",
-        variant: "success",
-      });
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: String(error),
-        variant: "danger",
-      });
-    }
-  }
-
-  return (
-    <Drawer
-      isOpen={isOpen}
-      onClose={onClose}
-      title={selected ? "Incident Report" : "File Incident"}
-      isSubmitting={isSubmitting}
-    >
-      <Form {...form}>
-        <form
-          className="space-y-2"
-          // id="drawer-form"
-          onSubmit={form.handleSubmit(onsubmit)}
+    return (
+        <Drawer
+            isOpen={isOpen}
+            onClose={()=>{ onClose(false); formReset(); }}
+            title={selected ? "Incident Report" : "File Incident"}
+            isSubmitting={isSubmitting}
+            unSubmittable={!form.watch('employee_id')}
+            footer={selected && <div/>}
         >
-          {!selected ? (
-            <EmployeeListForm isLoading={isLoading} employees={employees} />
-          ) : (
-            <UserMail
-              name={getEmpFullName(
-                data?.find((emp) => emp.id === selected.employee_id)!
-              )}
-              picture={
-                data?.find((emp) => emp.id === selected.employee_id)?.picture!
-              }
-            />
-          )}
-          <FormFields
-            items={[
-              {
-                name: "occurance_date",
-                type: "date-input",
-                label: "Occurance Date",
-                config: {readOnly: true},
-              },
-              {
-                name: "location",
-                label: "Location",
-                config: {readOnly: true},
-              },
-              {
-                name: "type",
-                label: "Type",
-                config: {readOnly: true},
-              },
-              {
-                name: "severity",
-                label: "Severity",
-                type: "radio-group",
-                config: {
-                  readOnly: true,
-                  options: [
-                    {
-                      label: "Minor",
-                      value: "minor",
-                    },
-                    {
-                      label: "Major",
-                      value: "major",
-                    },
-                    {
-                      label: "Critical",
-                      value: "critical",
-                    },
-                  ],
-                  orientation: "horizontal",
-                },
-              },
-              {
-                name: "description",
-                type: "text-area",
-                label: "Description",
-                config: {readOnly: true},
-              },
-              {
-                name: "comments",
-                type: "text-area",
-                label: "Comments",
-                config: {readOnly: true},
-              },
-            ]}
-          />
-        </form>
-      </Form>
-    </Drawer>
-  );
+            <Form {...form}>
+                <form className="space-y-2" id="drawer-form" onSubmit={form.handleSubmit(onsubmit)}>
+                    {!selected ? (
+                        <EmployeeListForm isLoading={isLoading} employees={employees} />
+                    ) : (
+                        <UserMail
+                            name={getEmpFullName(
+                                selected.trans_employees_dim_incident_reports_employee_idTotrans_employees
+                            )}
+                            picture={
+                                selected.trans_employees_dim_incident_reports_employee_idTotrans_employees.picture || ""
+                            }
+                        />
+                    )}
+                    <FormFields
+                        items={[
+                            {
+                                name: "occurance_date",
+                                type: "date-input",
+                                label: "Occurance Date",
+                                isRequired: true,
+                                inputDisabled: !!selected,
+                            },
+                            {
+                                name: "location",
+                                label: "Location",
+                                isRequired: true,
+                                placeholder: "e.g: On-site, Off-site",
+                                inputDisabled: !!selected,
+
+                            },
+                            {
+                                name: "type",
+                                label: "Type",
+                                placeholder: "e.g: Equipment Failure, Harrassment, Safety",
+                                isRequired: true,
+                                inputDisabled: !!selected,
+
+                            },
+                            {
+                                name: "severity",
+                                label: "Severity",
+                                type: "radio-group",
+                                isRequired: true,
+                                inputDisabled: !!selected,
+                                config: {
+                                    options: [
+                                        {
+                                            label: "Minor",
+                                            value: "minor",
+                                        },
+                                        {
+                                            label: "Major",
+                                            value: "major",
+                                        },
+                                        {
+                                            label: "Critical",
+                                            value: "critical",
+                                        },
+                                    ],
+                                    orientation: "horizontal",
+                                },
+                            },
+                            {
+                                name: "description",
+                                type: "text-area",
+                                label: "Description",
+                                inputDisabled: !!selected,
+                            },
+                            {
+                                name: "comments",
+                                type: "text-area",
+                                label: "Comments",
+                                inputDisabled: !!selected,
+                            },
+                            ...(selected
+                                ? [
+                                      {
+                                          name: "approver",
+                                          label: "Reporter",
+                                          Component: () => (
+                                              <Tooltip
+                                                  className="pointer-events-auto"
+                                                  content={getEmpFullName(
+                                                      selected.trans_employees_dim_incident_reports_reported_byTotrans_employees
+                                                  )}
+                                              >
+                                                  <Avatar
+                                                      isBordered
+                                                      radius="full"
+                                                      size="sm"
+                                                      src={
+                                                          selected
+                                                              ?.trans_employees_dim_incident_reports_reported_byTotrans_employees
+                                                              ?.picture ?? ""
+                                                      }
+                                                  />
+                                              </Tooltip>
+                                          ),
+                                      },
+                                  ]
+                                : []),
+                        ]}
+                    />
+                </form>
+            </Form>
+        </Drawer>
+    );
 }
 
 export default IncidentDrawer;
