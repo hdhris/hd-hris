97752f21e52ac4ece3df6c499a4228288edcb90e
MNizamD <ndaatumanong05@gmail.com>
2024-12-05 16:11:45 +0800

    added initial performance criteria UI

---
+++ app/(admin)/(core)/performance/criteria/page.tsx
@@ -0,0 +1,10 @@
+import CriteriaTable from '@/components/admin/performance/criteria/table'
+import React from 'react'
+
+async function Page() {
+  return (
+    <CriteriaTable/>
+  )
+}
+
+export default Page
---
+++ components/admin/performance/criteria/table.tsx
@@ -0,0 +1,50 @@
+"use client"
+import TableData from "@/components/tabledata/TableData";
+import { useQuery } from "@/services/queries";
+import { CriteriaDetail } from "@/types/performance/types";
+import { TableConfigProps } from "@/types/table/TableDataTypes";
+import { Chip } from "@nextui-org/react";
+import { toUpper } from "lodash";
+import React from "react";
+
+function CriteriaTable() {
+    const { data, isLoading, mutate } = useQuery<CriteriaDetail[]>("/api/admin/performance/criteria");
+
+    return <div className="flex w-full h-full">
+        <TableData config={config} items={data || []} isLoading={isLoading} />
+
+    </div>;
+}
+
+const config: TableConfigProps<CriteriaDetail> = {
+    columns: [
+        { uid: "name", name: "Name", sortable: true },
+        { uid: "desc", name: "Description" },
+        { uid: "type", name: "Type" },
+        { uid: "status", name: "Status" },
+    ],
+    rowCell: (item, columnKey) => {
+        switch (columnKey) {
+            case "name":
+                return <strong>{item.name}</strong>;
+            case "desc":
+                return <p>{item.description}</p>;
+            case "type":
+                return (
+                    <Chip color="primary" size="sm" variant="faded">
+                        {toUpper(item.ratings_json.type.replaceAll("-", " "))}
+                    </Chip>
+                );
+            case "status":
+                return (
+                    <Chip color={item.is_active ? "success" : "danger"} size="sm" variant="flat">
+                        {item.is_active ? "Active" : "In-active"}
+                    </Chip>
+                );
+            default:
+                return <></>;
+        }
+    },
+};
+
+export default CriteriaTable;
