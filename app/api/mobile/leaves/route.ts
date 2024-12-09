b078d256cb65c3973dd817a9ad4331507bd3af6a
MNizamD <ndaatumanong05@gmail.com>
2024-12-09 16:01:36 +0800

    added historical work schedule

---
+++ app/api/mobile/leaves/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/prisma";
+import { emp_rev_include } from "@/helper/include-emp-and-reviewr/include";
+
+export const dynamic = "force-dynamic";
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const employee_id = Number(searchParams.get("employee_id"));
+    try {
+        const leaves = await prisma.trans_leaves.findMany({
+            where: {
+                employee_id: 160,
+            },
+            include: {
+                trans_employees_trans_leaves_created_byTotrans_employees: {
+                    ...emp_rev_include.reviewer_detail,
+                },
+                trans_leave_types: {
+                    include: {
+                        ref_employment_status: true,
+                        ref_leave_type_details: true,
+                    }
+                },
+            }
+        })
+        return NextResponse.json(leaves);
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+    }
+}
