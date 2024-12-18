08c9a845bf89eaa9190453b44215632a406e520b
TechstyFrance <francejay724@gmail.com>
2024-12-18 20:48:01 +0800

    //fix f** issues

--- app/(admin)/(core)/trainings-and-seminars/emprecords/view/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ViewRecord from "@/components/admin/trainings-and-seminars/emprecords/ViewRecord";
-
-
-export default function Page({ params }: { params: { id: string } }) {
-    return <ViewRecord record_id={params.id} />;
-}
