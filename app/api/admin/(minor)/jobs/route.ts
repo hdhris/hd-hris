c27cc53a45b0cbeee006145c01ce3634eff3ed89
Xero <xerojohn7@gmail.com>
2024-12-18 11:00:02 +0800

    fixing maintenance mode

--- app/api/admin/(minor)/jobs/route.ts
+++ app/api/admin/(minor)/jobs/route.ts
@@ -1,15 +1,16 @@
 import prisma from "@/prisma/prisma"
+import { NextResponse } from "next/server"
 export async function GET(){
     try {
        const jobs = await prisma.ref_job_classes.findMany({
            where: {
                deleted_at: null
            },
-           select:{
-               
-           }
        })
-    } catch (error){
 
+        return NextResponse.json({success: true})
+    } catch (error){
+        console.log("Error: ", error)
+        return NextResponse.json({success: false, message: "Failed to fetch data"})
     }
 }
