ec455bc2044c4e6216e07c5cf2aa92dfc8ee7bef
Xero John <xerojohn7@gmail.com>
2025-01-04 00:11:01 +0800

    Fixing: dashboard ui

---
+++ app/api/admin/(core)/dashboard/route.ts
@@ -0,0 +1,26 @@
+import {getPrismaErrorMessage} from "@/server/errors/server-errors";
+import prisma from "@/prisma/prisma"
+import {NextResponse} from "next/server";
+import {isEmployeeAvailable} from "@/helper/employee/unavailableEmployee";
+
+export async function GET(){
+    try {
+        const [employee_count] = await prisma.$transaction([
+            prisma.trans_employees.findMany({
+                where: {
+                    deleted_at: null
+                },
+                select: {
+                    suspension_json: true,
+                    termination_json: true,
+                    resignation_json: true,
+                }
+            })
+        ])
+
+        const emp_count = employee_count.filter(emp => isEmployeeAvailable(emp as any, "resignation") && isEmployeeAvailable(emp as any, "termination")).length
+        return NextResponse.json({emp_count})
+    }catch(err){
+        return getPrismaErrorMessage(err)
+    }
+}
