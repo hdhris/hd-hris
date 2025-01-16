d7ac8a6a27181130945418bfd7fc25e6c6d2cae4
hdhris <hdhris2025@gmail.com>
2025-01-16 20:57:53 +0800

    removed: recaptcha

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 import {useRouter} from "next/navigation";
 import Login from "@/components/login/Login";
-import {auth} from "@/auth";
-import ReCaptchaProvider from "@/components/providers/GoogleRecaptcha";
 import {useSession} from "next-auth/react";
 
 export default function Home() {
@@ -10,11 +8,9 @@ export default function Home() {
     const router = useRouter()
     if (session) {
         router.replace("/dashboard")
-    }
 
+    }
     return (<main className="flex h-screen flex-col items-center">
-        <ReCaptchaProvider>
-            <Login/>
-        </ReCaptchaProvider>
+        <Login/>
     </main>);
 }
